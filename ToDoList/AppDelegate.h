ee411638a384d60123216cea8cb2e612080f3c8f
James Wang <java2enterprise@gmail.com>
2014-01-15 22:07:55 +0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by James Wang on 1/15/14.
+//  Copyright (c) 2014 James Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
